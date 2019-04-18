663a3d5b142f1c490b7ae624f976b4deb5d6deaf
Udochi Okeke <udochio@hotmail.com>
2019-04-18 16:21:16 -0400

    Update script.js
    Included "e" key in the menu obj and wrote the code to append the e list when 'e' key is pressed

--- script.js
+++ script.js
@@ -3,30 +3,33 @@
 let myform = document.getElementById("myform");
 let menulist = document.getElementById("menulist");
 let menuobj = {
-    'a' : ['a', 'á', 'à', 'ā', 'A', 'Á', 'À', 'Ā']
+    'a' : ['a', 'á', 'à', 'ā', 'A', 'Á', 'À', 'Ā'],
+    'e' : ['e', 'é', 'è', 'ē', 'E', 'É', 'È', 'Ē']
 }
+let keyArr = ['a', 'e', 'i', 'o', 'u', 'm', 'n'];
 
 function isVowel(userKey){
     if(userKey){
-        if(userKey.toLowerCase() === 'a'){
-            console.log("You typed a");
-            return true;
+            for(var menukeys in menuobj){
+                if(menukeys === userKey.toLowerCase()){
+                    console.log("You typed " + userKey.toLowerCase());
+                    return true;
+                }
+            }
         }else{
             console.log("You typed something else");
             return false;
-    }
-}else{
-    return false;
-}
-}
+        }
+ }
+
 
  function createmenu(event){
     var userKey = event.key;
-    var arr = menuobj.a;
+    if(isVowel(userKey)){
+        var arr = menuobj[userKey.toLowerCase()];
     var menulist = document.getElementById("menulist");
     var li = document.createElement("li");
     var textnode = "";
-    if(isVowel(userKey)){
         for(var i=0; i<arr.length; i++){
             textnode = document.createTextNode(arr[i]);
             li.appendChild(textnode);
