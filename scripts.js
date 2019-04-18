06de3cfb2f84193de2d7b5e2cdd7fcfe1d684110
Udochi Okeke <udochio@hotmail.com>
2019-04-18 13:46:26 -0400

    Update scripts.js to recognize keypresses

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
+let myform = document.getElementById("myform");
+let menuobj = {
+    'a' : ['a', 'A']
+}
 
+function getKey(event){
+    var mykey = event.key;
+    console.log('Your key was ' + mykey);
+}
+
+myform.addEventListener("keypress", getKey);
