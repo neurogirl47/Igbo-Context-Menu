455ed097ce397bfe4dca8aef199deda9262c8bcc
Udochi Okeke <udochio@hotmail.com>
2019-04-18 13:57:02 -0400

    Update script.js
    Added method to check if 'a' was pressed and console.log result

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+'use strict'
+
 let myform = document.getElementById("myform");
 let menuobj = {
     'a' : ['a', 'A']
@@ -6,6 +8,17 @@ let menuobj = {
 function getKey(event){
     var mykey = event.key;
     console.log('Your key was ' + mykey);
+    isVowel(mykey);
+}
+
+function isVowel(userKey){
+    if(userKey.toLowerCase() === 'a'){
+        console.log("You typed a");
+        return true;
+    }else{
+        console.log("You typed something else");
+        return false;
+    }
 }
 
 myform.addEventListener("keypress", getKey);
